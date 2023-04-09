61811ea844bfc89c65908be628a8b8a62e4b939e
Zakariya Hadrami <zakariyah.dev@gmail.com>
2023-04-09 21:08:34 +0200

    Add writer exception

--- include/ctq_writer.h
+++ include/ctq_writer.h
@@ -17,6 +17,23 @@ namespace CTQ {
  */
 int write(const std::string &src, const std::string &dst, const std::vector<std::string> &paths = {}, uint16_t cluster_size = 64000);
 
-}
+class writer_exception : public std::exception {
+public:
+    explicit writer_exception(const char* msg) : msg_{msg} {}
+    ~writer_exception() throw() override = default;
+
+    const char* what() const throw() override {
+        return msg_;
+    }
+
+  private:
+    const char* msg_;
+};
+
+#define CTQ_WRITER_TO_STR_(n) #n
+#define CTQ_WRITER_TO_STR(n) CTQ_WRITER_TO_STR_(n)
+#define CTQ_WRITER_THROW(msg) throw CTQ::writer_exception(__FILE__ " : " CTQ_WRITER_TO_STR(__LINE__) " : " msg)
+
+} // namespace CTQ
 
 #endif
